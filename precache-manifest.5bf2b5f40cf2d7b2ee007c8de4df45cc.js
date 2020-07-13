f7c58fea7e038ef109f2758f166abb45ec6583d3
adam <adam@adams-MacBook-Pro.local>
2020-07-13 14:35:13 +0300

    Updates

---
+++ precache-manifest.5bf2b5f40cf2d7b2ee007c8de4df45cc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d76e3c041539de89072df85ce4f11968",
+    "url": "/-React-Js-Git-engine/index.html"
+  },
+  {
+    "revision": "797a4b22a06d691a9ea5",
+    "url": "/-React-Js-Git-engine/static/css/main.a0c2f870.chunk.css"
+  },
+  {
+    "revision": "0c594807258b3e68d632",
+    "url": "/-React-Js-Git-engine/static/js/2.473b00c1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/-React-Js-Git-engine/static/js/2.473b00c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "797a4b22a06d691a9ea5",
+    "url": "/-React-Js-Git-engine/static/js/main.a927edc6.chunk.js"
+  },
+  {
+    "revision": "9facf4bdd358d8d7fbf4",
+    "url": "/-React-Js-Git-engine/static/js/runtime-main.c5102a43.js"
+  }
+]);
